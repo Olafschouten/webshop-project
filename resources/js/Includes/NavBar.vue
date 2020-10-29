a347931aea62fd8ab2648be11c1253e700d6aede
Olaf Schouten <olafschouten@MacBook-Pro-van-Olaf.local>
2020-10-29 10:40:21 +0100

    Add layout to all pages and navigation

---
+++ resources/js/Includes/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+      <jet-nav-link :href="route('dashboard')" :active="$page.currentRouteName == 'dashboard'">
+        Dashboard
+      </jet-nav-link>
+      <jet-nav-link :href="route('products')" :active="$page.currentRouteName == 'products'">
+        Products
+      </jet-nav-link>
+    </div>
+</template>
+
+<script>
+    import JetNavLink from './../Jetstream/NavLink'
+
+    export default {
+        components: {
+            JetNavLink,
+        },
+    }
+</script>
+
+<style>
+
+</style>
--- resources/js/Pages/Site/Products.vue
+++ resources/js/Pages/Site/Products.vue
@@ -1,26 +1,43 @@
 <template>
-  <div>
-    <h1>Products</h1>
-  
-      <div v-for="product in products" :key="product.id">
-        <p>{{ product.id }}</p>
-        <p>{{ product.title }}</p>
-        <p>$ {{ product.price }}</p>
-        <p>{{ product.url }}</p>
-        <br>
-        <ul>
-          <li><a href="">Add to cart</a></li>
-          <li><a href="">Link to Product</a></li>
-        </ul>
-        <br>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Products
+      </h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <h1>Products</h1>
+
+          <div v-for="product in products" :key="product.id">
+            <p>{{ product.id }}</p>
+            <p>{{ product.title }}</p>
+            <p>$ {{ product.price }}</p>
+            <p>{{ product.url }}</p>
+            <br />
+            <ul>
+              <li><a href="">Add to cart</a></li>
+              <li><a href="">Link to Product</a></li>
+            </ul>
+            <br />
+          </div>
+        </div>
       </div>
-  </div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
+import AppLayout from "./../../Layouts/AppLayout";
+
 export default {
+  components: {
+    AppLayout,
+  },
   props: {
     products: Array,
   },
-}
+};
 </script>
