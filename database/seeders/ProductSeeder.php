ddbc835685d2956d7a943ff36c311519566d5c35
Olaf Schouten <olafschouten@MacBook-Pro-van-Olaf.local>
2020-11-10 20:27:03 +0100

    update db connection

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'title' => $this->faker->firstName,
+            'description' => $this->faker->text($maxNbChars = 200),
+            'price' => $this->faker->numerify('##'),
+            'image' => 'storage/images/file.png',
+            'url' => '/#/product/',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
